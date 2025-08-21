dddd4001bb75d04fc2ed6495442d282140ac21ab
Denner Rondinely <denner.rondinely@gmail.com>
2025-08-21 14:33:48 -0300

    change build outputs

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import * as Repack from '@callstack/repack';
-import pkg from './package.json' with {type: "json"}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
